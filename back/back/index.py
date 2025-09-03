fec95471b9582dd996ef420dbf2feea0525691e8
tiaineno <olli.vj.tiainen@gmail.com>
2025-09-03 13:17:18 +0300

    Added backend conf

---
+++ back/back/index.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request, abort
+
+app = Flask(__name__)
+
+@app.get("/api/ping")
+def ping():
+    return jsonify({"ok": True, "message": "pong"})
+
+@app.post("/api/echo")
+def echo():
+    if not request.is_json:
+        abort(400, description="Body must be JSON")
+    data = request.get_json()
+    return jsonify({"you_sent": data})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
