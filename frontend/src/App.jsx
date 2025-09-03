f38e3722f72baf379a2fdb6cbfb2c72f990e6285
ellenra <ellen.rajala@gmail.com>
2025-09-03 13:44:21 +0300

    Improve frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./index.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [result, setResult] = useState("");
+
+  const pingBackend = async () => {
+    const res = await fetch("http://localhost:5000/api/ping");
+    const data = await res.json();
+    console.log(data);
+  };
+
+  pingBackend();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!inputValue) return;
+
+    try {
+      const res = await fetch("http://localhost:5000/api/echo", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message: inputValue }),
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to send data");
+      }
+
+      const data = await res.json();
+      setResult(data.you_sent.message);
+      setInputValue("");
+    } catch (err) {
+      console.error(err);
+      setResult("Error connecting to backend");
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="">
+      <form onSubmit={handleSubmit} className="">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          className=""
+        />
 
-export default App
+        <button type="submit" className="">
+          Submit
+        </button>
+      </form>
+
+      {result && (
+        <div className="">
+          <p className="">{result}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
