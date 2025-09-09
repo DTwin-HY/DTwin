8bb57f9da11344c84203f1c804aa9abb00f374e8
Karri Lumivirta <lumivirta.karri@hotmail.com>
2025-09-09 10:09:15 +0300

    db changes, fixing merge issues

--- back/main/index.py
+++ back/main/index.py
@@ -1,10 +1,9 @@
 from flask import Flask, jsonify, request, abort
 from flask_cors import CORS
 from main.chatgpt.chat import answer
-from chatgpt.chat import answer
-from sqlalchemy.sql import text
 from os import getenv
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
 CORS(app)
