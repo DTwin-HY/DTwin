9a4b67cca2bd5a0542ed99a28b701c5b2ebeef2c
Riku Nummi <rikunummi9@gmail.com>
2025-09-10 17:49:04 +0300

    Added signin page logic

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -4,3 +4,13 @@ export const signup = async ({ username, password }) => {
   const { data } = await axios.post("/signup", { username, password });
   return data; 
 };
+
+export const signin = async ({ username, password }) => {
+    console.log(username, password);   
+  const { data } = await axios.post("/signin", { username, password });
+  return data; 
+};
+
+export const signout = async () => {
+  return await axios.post("/signout"); 
+};
