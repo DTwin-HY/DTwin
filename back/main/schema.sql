c626eb5f4bea662366df9be91c6c8fa9721849e4
Riku Nummi <rikunummi9@gmail.com>
2025-09-10 13:35:51 +0300

    schema for users table

--- back/main/schema.sql
+++ back/main/schema.sql
@@ -3,3 +3,9 @@ CREATE TABLE logs (
     prompt TEXT,
     reply TEXT
 );
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
