02800d4f4ea83a41be17da4789de11dcf2807d03
August Rouvari <august.rouvari@helsinki.fi>
2025-09-11 15:54:59 +0300

    Add guestroute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import SignUp from './components/SignUp'
 import Login from './components/SignIn'
-import PrivateRoute from "./components/PrivateRoute"
+import PrivateRoute from './components/PrivateRoute'
+import GuestRoute from './components/GuestRoute'
 import "./index.css";
 
 
@@ -10,8 +11,8 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
-      <Route path="/signin" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signin" element={<GuestRoute><Login /></GuestRoute>} />
+      <Route path="/signup" element={<GuestRoute><SignUp /></GuestRoute>} />
     </Routes>
   );
 };
