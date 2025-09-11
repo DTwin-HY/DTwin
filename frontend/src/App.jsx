2f7056afc932068a5bfd7e892688c7e2e046f28c
August Rouvari <august.rouvari@helsinki.fi>
2025-09-11 15:48:19 +0300

    Add privateroute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import SignUp from './components/SignUp'
 import Login from './components/SignIn'
+import PrivateRoute from "./components/PrivateRoute"
 import "./index.css";
 
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
       <Route path="/signin" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
     </Routes>
