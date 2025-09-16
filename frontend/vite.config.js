7d32b9cdd46821374890f63157205e949d4b4eb9
Karri Lumivirta <lumivirta.karri@hotmail.com>
2025-09-16 11:46:38 +0300

    Update config to retrieve api URL from environment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import autoprefixer from "autoprefixer";
+// https://vite.dev/config/
+//export default defineConfig({
+//  plugins: [react(), tailwindcss()],
+//})
+//import { defineConfig, loadEnv } from "vite";
+//import react from "@vitejs/plugin-react";
+//import tailwindcss from "tailwindcss";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    define: {
+      "process.env": env,
+    },
+    plugins: [react(),tailwindcss()],
+    server: {
+      proxy: {
+        "/api/": process.env.VITE_BACKEND_URL,
+      },
+    },
+    css: {
+      postcss: {
+        plugins: [tailwindcss, autoprefixer],
+      },
+    },
+  };
+});
