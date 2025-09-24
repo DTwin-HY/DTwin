36a8915fa16c4ecf051f366278cf474c53059fe3
Riku Nummi <rikunummi9@gmail.com>
2025-09-24 19:20:51 +0300

    Add last of the tutorial on supervisor agent

--- back/main/chatgpt/supervisor_testing.py
+++ back/main/chatgpt/supervisor_testing.py
@@ -164,19 +164,80 @@ supervisor = (
     .compile()
 )
 
-for chunk in supervisor.stream(
+def create_task_description_handoff_tool(
+    *, agent_name: str, description: str | None = None
+):
+    name = f"transfer_to_{agent_name}"
+    description = description or f"Ask {agent_name} for help."
+
+    @tool(name, description=description)
+    def handoff_tool(
+        # this is populated by the supervisor LLM
+        task_description: Annotated[
+            str,
+            "Description of what the next agent should do, including all of the relevant context.",
+        ],
+        # these parameters are ignored by the LLM
+        state: Annotated[MessagesState, InjectedState],
+    ) -> Command:
+        task_description_message = {"role": "user", "content": task_description}
+        agent_input = {**state, "messages": [task_description_message]}
+        return Command(
+            goto=[Send(agent_name, agent_input)],
+            graph=Command.PARENT,
+        )
+
+    return handoff_tool
+
+
+assign_to_research_agent_with_description = create_task_description_handoff_tool(
+    agent_name="research_agent",
+    description="Assign task to a researcher agent.",
+)
+
+assign_to_math_agent_with_description = create_task_description_handoff_tool(
+    agent_name="math_agent",
+    description="Assign task to a math agent.",
+)
+
+supervisor_agent_with_description = create_react_agent(
+    model="openai:gpt-4.1",
+    tools=[
+        assign_to_research_agent_with_description,
+        assign_to_math_agent_with_description,
+    ],
+    prompt=(
+        "You are a supervisor managing two agents:\n"
+        "- a research agent. Assign research-related tasks to this assistant\n"
+        "- a math agent. Assign math-related tasks to this assistant\n"
+        "Assign work to one agent at a time, do not call agents in parallel.\n"
+        "Do not do any work yourself."
+    ),
+    name="supervisor",
+)
+
+supervisor_with_description = (
+    StateGraph(MessagesState)
+    .add_node(
+        supervisor_agent_with_description, destinations=("research_agent", "math_agent")
+    )
+    .add_node(research_agent)
+    .add_node(math_agent)
+    .add_edge(START, "supervisor")
+    .add_edge("research_agent", "supervisor")
+    .add_edge("math_agent", "supervisor")
+    .compile()
+)
+
+for chunk in supervisor_with_description.stream(
     {
         "messages": [
             {
                 "role": "user",
-                "content": "find Finlands and Uusimaas GDP in 2025. what % of Finlands GDP was Uusimaas?",
+                "content": "find Finlands and Uusimaas GDP in 2024. what % of Finlands GDP was Uusimaas?",
             }
         ]
     },
+    subgraphs=True,
 ):
     pretty_print_messages(chunk, last_message=True)
-
-final_message_history = chunk["supervisor"]["messages"]
-
-for message in final_message_history:
-    message.pretty_print()
