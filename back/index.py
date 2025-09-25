64c1242f0141db15fc39eb9a8d21d370809b3a88
jensjvh <43621451+jensjvh@users.noreply.github.com>
2025-09-25 13:26:42 +0300

    Fix db table creation

--- back/index.py
+++ back/index.py
@@ -21,9 +21,9 @@ bcrypt = Bcrypt(app)
 from .main import routes
 
 def start():
+    with app.app_context():
+        db.create_all()
     app.run(host="0.0.0.0", port=5000, debug=True)
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     start()
