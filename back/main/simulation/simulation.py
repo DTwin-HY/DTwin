6cfe8055e6307fba3f65536256b31e2bc52fe0b9
Karri Lumivirta <lumivirta.karri@hotmail.com>
2025-10-09 09:38:28 +0300

    Remove old files and cleanup for project pivot
    Co-authored-by: rigozu9 <rikunummi9@gmail.com>
    Co-authored-by: tiaineno <olli.vj.tiainen@gmail.com>

--- back/main/simulation/simulation.py
+++
@@ -1,9 +0,0 @@
-def run_simulation():
-    """
-    for running a single conversation between a seller and a customer
-    initializes its own conversation-specific state, runs the conversation and updates the general state
-    """
-    return 
-
-if __name__ == "__main__":
-    run_simulation()
--- back/main/simulation/state.py
+++ back/main/simulation/state.py
@@ -1,30 +1,11 @@
 from typing_extensions import TypedDict, Annotated, List, Dict
 from langgraph.graph.message import add_messages
 
-class LogEntry(TypedDict):
-    """
-    Object used inside logs list
-    """
-    product_id: str
-    amount: int
-    event_type: str
-    timestamp: str
-
-class InventoryItem(TypedDict):
-    """
-    Object used inside inventory dict
-    """
-    product_id: int
-    name: str
-    quantity: int
-    price: float
 
 class GeneralState(TypedDict):
     """
     general state
     """
-    cash_register: float
-    inventory: Dict[int, InventoryItem]
-    logs: List[LogEntry]
-    is_raining: bool
-    supervisor_conversation: Annotated[list, add_messages]
+#TODO: make a state
+
+
