f4a5d640a00b67aeb26787ac2295340e3451f305
jensjvh <43621451+jensjvh@users.noreply.github.com>
2025-10-27 08:31:09 +0200

    Add new versions of sql agent and storage agent

---
+++ back/src/services/storage_agent_v2.py
@@ -0,0 +1,32 @@
+from typing import Annotated
+
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
+from langgraph.prebuilt import create_react_agent
+
+from .sql_agent_v2 import sql_agent_tool
+
+load_dotenv()
+
+
+storage_react_agent = create_react_agent(
+    name="storage_agent",
+    model="openai:gpt-4o-mini",  # preferably 4o-mini or 5-mini because otherwise queries take too long
+    tools=[sql_agent_tool],
+    prompt=(
+        "You are an **inventory management agent**.\n\n"
+        "**Response rules:**\n"
+        "- Always respond strictly in JSON format.\n"
+        "- No markdown, no natural language text, no explanations.\n"
+        '- Example valid response: `{ "A100": 50 }`\n'
+        '- If you cannot find data, respond with `{ "error": "reason here" }`.\n'
+    ),
+)
+
+
+if __name__ == "__main__":
+    for step in storage_react_agent.stream(
+        {"messages": [HumanMessage(content="Sum up all the prices in the inventory")]},
+        stream_mode="values",
+    ):
+        print(step["messages"][-1].content)
