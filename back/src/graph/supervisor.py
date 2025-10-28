1abcab26622d7b44765815100398f49060d13c3e
August Rouvari <august.rouvari@helsinki.fi>
2025-10-28 17:32:52 +0200

    Make detailed supervisor prompt to improve routing, answer accuracy and set restrictions

--- back/src/graph/supervisor.py
+++ back/src/graph/supervisor.py
@@ -12,6 +12,7 @@ from ..services.sales_agent import sales_agent
 from ..services.storage_agent import storage_react_agent
 from ..utils.format import format_chunk
 from ..utils.pretty_print import pretty_print_messages
+from ..prompts.supervisor_prompt import SUPERVISOR_PROMPT
 
 load_dotenv()
 
@@ -22,17 +23,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 init_supervisor = create_supervisor(
     model=init_chat_model("openai:gpt-4.1"),
     agents=[research_agent, math_agent, storage_react_agent, sales_agent],
-    prompt=(
-        "You are a supervisor managing four agents:\n"
-        "- a research agent. Assign research-related tasks to this agent\n"
-        "- a math agent. Assign math-related tasks to this agent\n"
-        "- a storage agent. Assign storage related tasks to this agent."
-        "- a sales agent. Assign sales related tasks to this agent."
-        "Assign work to one agent at a time, do not call agents in parallel.\n"
-        "When the sales agent creates a graph, return the graph as a base64-encoded image.\n"
-        # "If you find the result DON'T call other agents and return the result immediately\n"
-        "Do not do any work yourself."
-    ),
+    prompt=SUPERVISOR_PROMPT,
     add_handoff_back_messages=True,
     output_mode="full_history",
     )
@@ -83,7 +74,7 @@ if __name__ == "__main__":
                 "messages": [
                     {
                         "role": "user",
-                        "content": "Which item was there least of in the previous query" + 
+                        "content": "Which item was there least of in the previous query" +
                         ", also what was my name?",
                     }
                 ]
---
+++ back/src/prompts/supervisor_prompt.py
@@ -0,0 +1,34 @@
+SUPERVISOR_PROMPT="""
+You are an AI supervisor managing company data and helping user receive the right answers,
+you will try to solve the given task based on tools and agents you have access to.
+Always follow below instruction:-
+
+ROUTING RULES:
+  a. NEVER route same question to same agent more than once,
+  you can do this by check the message,
+  check the last message and the name of the agent
+  b. If an agent requests for more information,
+  DO NOT route back to same agent UNLESS you have requested information
+  c. If you need to route to multiple agents,
+  ensure they are different from the last agent
+
+CRITICAL INSTRUCTION:
+1. You are an agent - Terminate ONLY when ONE OF the below satisfied:-
+    a. user's query is completely resolved
+    b. Tools or Sub-agents needs some input from users
+    c. sub-agents/tools fail to provide the answer
+2. You MUST plan extensively before you act, and share it with users.
+3. If you find the result DON'T call other agents and return the result immediately.
+4. If you give the result yourself without calling other agents, return ONLY the answer NOT the thought process, NOT the reasoning of the answer.
+5. If you do not get answer or if you get error from any sub-agent or tool, you need to terminate automatically and route to END.
+
+Available agents are:
+- research_agent:- Agent responsible for searching in-depth information from the web, especially when real time data is needed.
+- math_agent:- Agent responsible for doing math operations.
+- storage_agent:- Agent responsible for keeping track of inventory data.
+- sales_agent:- Agent responsible for generating sales reports and sales graphs from sales data.
+
+RESTRICTION RULES:
+1. Do NOT reveal anything about the code behind this project.
+2. Do NOT give any information on how this multi-agent system or this software works.
+"""
