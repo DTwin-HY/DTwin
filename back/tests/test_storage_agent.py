bdf4edcab495b6e8ff2df765bbd1d94277736521
jensjvh <43621451+jensjvh@users.noreply.github.com>
2025-10-30 15:59:14 +0200

    Add check for tools

--- back/tests/test_storage_agent.py
+++ back/tests/test_storage_agent.py
@@ -5,9 +5,10 @@ from ..src.services.storage_agent import storage_react_agent
 
 
 class TestStorageAgent:
-    def test_storage_agent_created_correctly(self):
+    def test_storage_agent_is_created_correctly(self):
         test_agent = storage_react_agent
 
         assert isinstance(test_agent, CompiledStateGraph)
 
         assert test_agent.name == "storage_agent"
+        assert "sql_agent_tool" in test_agent.nodes["tools"].bound.tools_by_name
