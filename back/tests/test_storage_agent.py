340fee7895b7dc9b3a4d752d033686784791cd34
jensjvh <43621451+jensjvh@users.noreply.github.com>
2025-10-30 15:17:43 +0200

    Add a test for creating the storage agent

--- back/tests/test_storage_agent.py
+++ back/tests/test_storage_agent.py
@@ -5,10 +5,17 @@ from ..src.services.storage_agent import storage_react_agent
 
 
 class TestStorageAgent:
+<<<<<<< HEAD
     def test_storage_agent_is_created_correctly(self):
+=======
+    def test_storage_agent_created_correctly(self):
+>>>>>>> 3a3e73e (Add a test for creating the storage agent)
         test_agent = storage_react_agent
 
         assert isinstance(test_agent, CompiledStateGraph)
 
         assert test_agent.name == "storage_agent"
+<<<<<<< HEAD
         assert "sql_agent_tool" in test_agent.nodes["tools"].bound.tools_by_name
+=======
+>>>>>>> 3a3e73e (Add a test for creating the storage agent)
