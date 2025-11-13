dc36d058112487faf0d17cd111524847208ca944
jensjvh <43621451+jensjvh@users.noreply.github.com>
2025-11-13 14:35:23 +0200

    Add imports to supervisor and fix formatting

--- back/src/graph/supervisor.py
+++ back/src/graph/supervisor.py
@@ -2,10 +2,11 @@ import json
 import os
 
 from dotenv import load_dotenv
-from langgraph.checkpoint.postgres import PostgresSaver
 from langchain.agents import create_agent
+from langgraph.checkpoint.postgres import PostgresSaver
 
 from ..services.math_agent import math_agent_tool
+from ..services.mcp_client import mcp_agent_tool
 from ..services.research_agent import research_agent_tool
 from ..services.sales_agent import sales_agent_tool
 from ..services.storage_agent import storage_agent_tool
@@ -21,7 +22,13 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 init_supervisor = create_agent(
     model="openai:gpt-4.1",
-    tools=[research_agent_tool, math_agent_tool, storage_agent_tool, sales_agent_tool],
+    tools=[
+        research_agent_tool,
+        math_agent_tool,
+        storage_agent_tool,
+        sales_agent_tool,
+        mcp_agent_tool,
+    ],
     system_prompt=supervisor_prompt,
 )
 
@@ -35,19 +42,18 @@ def stream_process(prompt: str, thread_id: str = "3"):
     """
     config = {"configurable": {"thread_id": thread_id}}
 
-    with PostgresSaver.from_conn_string(DATABASE_URL) as checkpointer: # pragma: no cover
-        checkpointer.setup() # pragma: no cover
-        supervisor = init_supervisor # pragma: no cover
+    with PostgresSaver.from_conn_string(DATABASE_URL) as checkpointer:  # pragma: no cover
+        checkpointer.setup()  # pragma: no cover
+        supervisor = init_supervisor  # pragma: no cover
 
         for chunk in supervisor.stream(
-            {"messages": [{"role": "user", "content": prompt}]},
-            stream_mode="updates"
+            {"messages": [{"role": "user", "content": prompt}]}, stream_mode="updates"
         ):
             output = format_chunk(chunk)  # stream the output to the frontend
             yield f"data: {json.dumps(output)}\n\n"
 
 
-if __name__ == "__main__": # pragma: no cover
+if __name__ == "__main__":  # pragma: no cover
     with PostgresSaver.from_conn_string(DATABASE_URL) as checkpointer:
         checkpointer.setup()
 
