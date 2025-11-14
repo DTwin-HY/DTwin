75c59a345ed54cc617b04a8e1e36482f6465b312
Karri Lumivirta <lumivirta.karri@hotmail.com>
2025-11-14 12:33:22 +0200

    Add reading data from disk location to simulation agent and lin reg tools, fix df issue with state in checkpointing

--- back/src/services/simulation/lin_reg_tool.py
+++ back/src/services/simulation/lin_reg_tool.py
@@ -2,30 +2,40 @@ from typing import Any, Dict, List
 
 import pandas as pd
 from langchain_core.tools import StructuredTool
-from langchain.tools import tool
+from langchain.tools import tool, ToolRuntime
+from langgraph.types import Command
 
 from ...services.simulation.lin_reg_graph import build_lin_reg_graph
 
+
+def csv_fetch(dataframe_path: str) -> str:
+    """
+    Tool to check the value of dataframe saved as csv. Used in development only.
+    Parameters:
+    dataframe_path: str : path to the dataframe csv file.
+    """
+    path = dataframe_path
+    dataframe = pd.read_csv(path).to_dict() 
+    print("fetched dataframe from csv file:\n", dataframe) 
+    
+    return dataframe
+
+
 @tool
-def lin_reg_tool(data: List[Dict[str, Any]]) -> Dict[str, Any]:
+def lin_reg_tool(data_location: str) -> Dict[str, Any]:
     """Runs the linear regression simulation graph on given data. \n
-    Expects data with columns: sales, sunny, price, customers"""
-    df = pd.DataFrame(data)
+    Expects a string with data location on file"""
+
+    try:
+        data = csv_fetch(data_location)
+    except:
+        return "Failed to load data."
+
     graph = build_lin_reg_graph()
-    result = graph.invoke({"df": df})
+    result = graph.invoke({"df": data})
     
     return result.get("results", {"errors": result.get("errors", [])})
 
-
-#lin_req_tool = StructuredTool.from_function(
-#    func=lin_reg_tool,
-#    name="linear_regression_simulation",
-#    description=(
-#        "Run a linear regression simulation on product sales data. "
-#        "Expects data with columns: sales, sunny, price, customers."
-#    ),
-#)
-#
 
 if __name__ == "__main__":
     from src.data.data_analysis_test_data import create_product_sales_data
