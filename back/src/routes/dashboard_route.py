e7bb703ae4325c842bf83d4c6cdef97670d9912d
Karri Lumivirta <lumivirta.karri@hotmail.com>
2025-11-24 13:49:12 +0200

    Add intial backend support for dashboard data

---
+++ back/src/routes/dashboard_route.py
@@ -0,0 +1,17 @@
+from flask import jsonify
+
+from ..utils.dashboard import fetch_dashboard_data
+from ..extensions import db
+from ..index import app
+
+
+@app.get("/api/dashboard-data")
+def get_dashboard_data():
+
+    try:
+        data = fetch_dashboard_data(0,1)
+        return jsonify(data)
+    
+    except Exception as e:
+
+        return jsonify({"error": str(e)})
