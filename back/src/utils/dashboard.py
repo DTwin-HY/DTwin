2e933685bc4e4845deaeef4c0084b514851ff2f1
Karri Lumivirta <lumivirta.karri@hotmail.com>
2025-11-24 15:58:40 +0200

    Add default graph data

--- back/src/utils/dashboard.py
+++ back/src/utils/dashboard.py
@@ -2,22 +2,46 @@ from flask import jsonify
 from ..index import app
 
 
+SALES_GRAPH_DATA = {
+                "current_quarter": {0:20, 1:20, 2:20, 3:20, 4:20,5:20},
+                "previous_quarter": {0:20, 1:20, 2:20, 3:20, 4:20,5:20},
+                "ytd": {0:20, 1:20, 2:20, 3:20, 4:20,5:20},
+}
+
 SALES = {
         "current_quarter": {"amount":3300, "growth": 0.10},
         "previous_quarter":{"amount":3000, "growth": 0.13},
-        "ytd": {"amount":11500, "growth": 0.09}
+        "ytd": {"amount":11500, "growth": 0.09},
+        "raw_graph_data": SALES_GRAPH_DATA
+
         }
+
+
+REVENUE_GRAPH_DATA = {
+                "current_quarter": {0:20, 1:20, 2:20, 3:20, 4:20,5:20},
+                "previous_quarter": {0:20, 1:20, 2:20, 3:20, 4:20,5:20},
+                "ytd": {0:20, 1:20, 2:20, 3:20, 4:20,5:20},
+}
 
 REVENUE = {
         "current_quarter": {"amount":22000, "growth": 0.10},
         "previous_quarter":{"amount":20000, "growth": 0.13},
-        "ytd": {"amount":120000, "growth": 0.09}
+        "ytd": {"amount":120000, "growth": 0.09},
+        "raw_graph_data": REVENUE_GRAPH_DATA
         }
+
+
+TRANSACTIONS_GRAPH_DATA = {
+                "current_quarter": {0:20, 1:20, 2:20, 3:20, 4:20,5:20},
+                "previous_quarter": {0:20, 1:20, 2:20, 3:20, 4:20,5:20},
+                "ytd": {0:20, 1:20, 2:20, 3:20, 4:20,5:20},
+}
 
 TRANSACTIONS = {
         "current_quarter": {"amount":1100, "growth": 0.10},
         "previous_quarter":{"amount":1000, "growth": 0.13},
-        "ytd": {"amount":3750, "growth": 0.09}
+        "ytd": {"amount":3750, "growth": 0.09},
+        "raw_graph_data": TRANSACTIONS_GRAPH_DATA
         }
 
 
