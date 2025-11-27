ab54eb12bc5f1d892c9913afe9f84bc9d9f2ebc4
Karri Lumivirta <lumivirta.karri@hotmail.com>
2025-11-27 12:47:38 +0200

    Cleanup code, fix lint issues, add docstrings

--- back/src/database/sales.py
+++ back/src/database/sales.py
@@ -35,104 +35,133 @@ def fetch_sales_data(start, end):
 
 
 def query_sales(start,end):
+    """
+    Query sales data for date, quantity and revenue per day.
+    Returns a SQLALchemy query result object.
+    Args:
+        start (str): start date as string
+        end (str): end date as string
+    """
 
-        sql = text(
-                """
-                SELECT 
-	                timestamp,
-	                SUM(quantity) as quantity,
-	                SUM(amount)  as revenue 
-                FROM 
-	                sales
-                WHERE 
-	                timestamp BETWEEN :start_date AND :end_date
-                GROUP BY 
-	                timestamp
-                ORDER BY
-                	timestamp; 
-                """
-        )
-        try:
-            result = db.session.execute(sql, {"start_date":start, "end_date":end}).fetchall()
+    sql = text(
+        """
+        SELECT 
+                timestamp,
+                SUM(quantity) as quantity,
+                SUM(amount)  as revenue 
+        FROM 
+                sales
+        WHERE 
+                timestamp BETWEEN :start_date AND :end_date
+        GROUP BY 
+                timestamp
+        ORDER BY
+                timestamp; 
+        """
+    )
+    try:
+        result = db.session.execute(sql, {"start_date":start, "end_date":end}).fetchall()
 
-            return result
+        return result
 
-        except Exception:
-            db.session.rollback()
-            raise
+    except Exception:
+        db.session.rollback()
+        raise
 
-def query_timeperiod_sales(start, end, time):
-        
-        sql = text(
-                """
-                SELECT 
-	                date_part(:time_interval, timestamp) as timeperiod,
-	                SUM(quantity) as quantity,
-	                SUM(amount)  as revenue 
-                FROM 
-	                sales
-                WHERE 
-	                timestamp BETWEEN :start_date AND :end_date
-                GROUP BY 
-	                timeperiod
-                ORDER BY
-                	timeperiod; 
-                """
-        )
-        try:
-            result = db.session.execute(sql, {"start_date":start, "end_date":end, 'time_interval':time}).fetchall()
+def query_timeperiod_sales(start, end, time_interval):
+    """
+    Query sales data for quantity and revenue per selected time interval. Returns a query object.
+    Args:
+        start (str): start date as string
+        end (str): end date as string
+        time_interval (str):time interval used by postgres date_part(), e.g.'week' or 'month'
+    """
+    sql = text(
+        """
+        SELECT 
+            date_part(:time_interval, timestamp) as timeperiod,
+            SUM(quantity) as quantity,
+            SUM(amount)  as revenue 
+        FROM 
+            sales
+        WHERE 
+            timestamp BETWEEN :start_date AND :end_date
+        GROUP BY 
+            timeperiod
+        ORDER BY
+            timeperiod; 
+        """
+    )
+    try:
+        result = db.session.execute(sql, {"start_date":start, "end_date":end,
+                                    'time_interval':time_interval}).fetchall()
 
-            return result
+        return result
 
-        except Exception:
-            db.session.rollback()
-            raise
+    except Exception:
+        db.session.rollback()
+        raise
 
 
 def query_transactions(start, end):
-        sql = text(
-                """
-                SELECT 
-                        timestamp, 
-                        COUNT(transaction_id) as transactions
-                FROM 
-                        sales 
-                WHERE 
-                        timestamp BETWEEN :start_date AND :end_date 
-                GROUP BY 
-                        timestamp 
-                ORDER BY 
-                        timestamp;
-                """
-                )
-        try:
-            result = db.session.execute(sql, {"start_date":start, "end_date":end}).fetchall()
-            return result
+    """
+    Query daily transactions over selected time period.
+    Args:
+        start (str): Start date as string
+        end (str): End date as string
+    """
+    sql = text(
+        """
+        SELECT 
+            timestamp, 
+            COUNT(transaction_id) as transactions
+        FROM 
+            sales 
+        WHERE 
+            timestamp BETWEEN :start_date AND :end_date 
+        GROUP BY 
+            timestamp 
+        ORDER BY 
+            timestamp;
+        """
+    )
+    try:
+        result = db.session.execute(sql, {"start_date":start, "end_date":end}).fetchall()
+        return result
 
-        except Exception:
-            db.session.rollback()
-            raise
-        
-def query_timeperiod_transactions(start, end, time):
-        sql = text(
-                """
-                SELECT 
-                        date_part(:time_interval, timestamp) as timeperiod, 
-                        COUNT(transaction_id) as transactions
-                FROM 
-                        sales 
-                WHERE 
-                        timestamp BETWEEN :start_date AND :end_date 
-                GROUP BY 
-                        timeperiod 
-                ORDER BY 
-                        timeperiod;
-                """
-                )
-        try:
-            result = db.session.execute(sql, {"start_date":start, "end_date":end, 'time_interval': time}).fetchall()
-            return result
+    except Exception:
+        db.session.rollback()
+        raise
 
-        except Exception:
-            db.session.rollback()
-            raise
+def query_timeperiod_transactions(start, end, time_interval):
+    """
+    Query transactions over selected time interval, e.g. daily, weekly or monthly.
+    Args:
+        start (str): start date of query
+        end (str): end date of query
+        time_interval (str): Selected postgres time interval, e.g. 'day', 'week' or 'month'
+
+    """
+    sql = text(
+        """
+        SELECT 
+                date_part(:time_interval, timestamp) as timeperiod, 
+                COUNT(transaction_id) as transactions
+        FROM 
+                sales 
+        WHERE 
+                timestamp BETWEEN :start_date AND :end_date 
+        GROUP BY 
+                timeperiod 
+        ORDER BY 
+                timeperiod;
+        """
+    )
+    try:
+        result = db.session.execute(sql, {"start_date":start, "end_date":end,
+                                    'time_interval': time_interval}).fetchall()
+        return result
+
+    except Exception:
+        db.session.rollback()
+        raise
