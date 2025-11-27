dbcdbed8f164a25360a0a51645c30af692a6c0f7
Heidi Tapani <heidi.tapani@helsinki.fi>
2025-11-27 17:48:54 +0200

    Remove SalesCard from view

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,28 +1,20 @@
 import Chatbot from './Chatbot';
 import InfoPanel from './InfoPanel';
-import SalesCard from './SalesCard';
 import Dashboard from './Dashboard';
 
 const Home = () => {
   return (
-    <div className="bg-gray-100">
-      <div className="w-full">
+    <div className="bg-gray-100 min-h-screen flex flex-col items-center">
+      <div className="w-full max-w-[1500px] px-4 sm:px-6 lg:px-8">
         <InfoPanel />
       </div>
 
-      <div className="mx-auto grid grid-cols-1 px-4 sm:px-6 lg:grid-cols-2 lg:px-8 lg:py-6">
-        <div className="lg:col-span-1">
-          <SalesCard className="h-full w-full" />
-        </div>
+      <div className="w-full max-w-[1500px] px-4 sm:px-6 lg:px-8 py-4">
+        <Dashboard />
+      </div>
 
-        <div className="lg:col-span-1">
-          <div>
-            <Dashboard />
-          </div>
-          <div className="lg:sticky">
-            <Chatbot />
-          </div>
-        </div>
+      <div className="w-full max-w-[1500px] px-4 sm:px-6 lg:px-8 py-4">
+        <Chatbot />
       </div>
     </div>
   );
