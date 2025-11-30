69d1cad8c34c001ed7cfca2a4d448f24a2d2c9e6
ellenra <ellen.rajala@gmail.com>
2025-11-30 16:08:49 +0200

    Add complete landing page

---
+++ frontend/src/components/LandingPage/Header.jsx
@@ -0,0 +1,30 @@
+import { motion } from 'motion/react';
+import { useNavigate } from 'react-router-dom';
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  return (
+    <motion.header
+      initial={{ y: -100 }} // Start above screen
+      animate={{ y: 0 }} // Drop down on page load
+      transition={{ duration: 0.6 }} // Smooth drop
+      className="fixed top-0 right-0 left-0 z-50 bg-transparent duration-300"
+    >
+      <div className="container mx-auto flex h-16 items-center justify-between">
+        <motion.div whileHover={{ scale: 1.05 }} className="cursor-pointer">
+          <span className="text-lg font-bold hover:cursor-pointer">DTwin</span>
+        </motion.div>
+
+        <button
+          onClick={() => navigate('/dashboard')}
+          className="rounded-lg font-medium hover:cursor-pointer"
+        >
+          Explore Demo
+        </button>
+      </div>
+    </motion.header>
+  );
+};
+
+export default Header;
