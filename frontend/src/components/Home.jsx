936637655f228e70ccd62835f1903d5917043fc1
Karri Lumivirta <lumivirta.karri@hotmail.com>
2025-12-01 13:26:40 +0200

    Remove infopanel from dashboard view

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,10 +5,6 @@ import Dashboard from './Dashboard';
 const Home = () => {
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col items-center">
-      <div className="w-full max-w-[1500px] px-4 sm:px-6 lg:px-8">
-        <InfoPanel />
-      </div>
-
       <div className="w-full max-w-[1500px] px-4 sm:px-6 lg:px-8 py-4">
         <Dashboard />
       </div>
