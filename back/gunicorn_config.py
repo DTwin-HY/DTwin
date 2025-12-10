eb5be51dfef247889f5c3cc4baa5a7f47a9b3f91
jensjvh <43621451+jensjvh@users.noreply.github.com>
2025-12-10 14:52:59 +0200

    Gunicorn hotfix

--- back/gunicorn_config.py
+++ back/gunicorn_config.py
@@ -3,6 +3,10 @@ import os
 
 workers = int(os.environ.get("GUNICORN_PROCESSES", "2"))
 
+timeout = 1200
+
+graceful_timeout = 120
+
 threads = int(os.environ.get("GUNICORN_THREADS", "4"))
 
 bind = os.environ.get("GUNICORN_BIND", "0.0.0.0:5000")
